52ea43f787585b13d04325e1467f9cf5985807c1
z.hayrullah <z.hayrullah@imtilak.net>
2021-05-01 19:28:28 +0300

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">Who is Zaher Khirullah ?</div>
+    <div class="panel" id="home">
+        <h1 align="center">Hi there 👋, I'm Zaher</h1>
 
-            <div class="panel-body">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <div class="panel" id="home">
-                    <div class="content_section">
-                        <h2> I`m Zaher, Back-end web developer</h2>
-                        <img src="/assets/images/tooplate_image_01.jpg" alt="Image 01" class="image_wrapper image_fl"/>
-                        <p>
-                            <em>
+        <p align="left"><img src="https://komarev.com/ghpvc/?username=zaherkhirullah&label=Profile%20Views&color=0e75b6&style=flat" alt="Zaher Khirullah"/>
+            <a href="https://zaherr.com" target="blank"><img src="https://img.shields.io/website/http/www.zaherr.com" alt="Zaher Khirullah"/></a>
+            <a href="https://twitter.com/zaherkhirullah" target="blank"><img src="https://img.shields.io/twitter/follow/zaherkhirullah?logo=twitter&style=badge" alt="Zaher Khirullah"/></a></p>
 
-                            </em>
-                        </p>
-                        <p>My name is <a rel="nofollow" href="https://www.zaherr.com">Zaher Khirullah</a>, a Graduated Computer engineer living in Turkey. Also working in Imtilak Real Estate in Istanbul as a Back-end developer, Coding is my passion, my job is my hobby and always open to learning new tech stuff.
-                        </p>
-                    </div>
-
-                    <div class="content_section last_section">
-                        <h2>Our Services</h2>
-                        <ul class="service_list">
-
-                            <li><a href="#" class="service_one">Web Development</a></li>
-                            <li><a href="#" class="service_two">Build Responsive Sites</a></li>
-                            <li><a href="#" class="service_three">Mobile Applications </a></li>
-                            <li><a href="#" class="service_four">Hosting services</a></li>
-{{--                            <li><a href="#" class="service_five">Maecenas posuere fringilla ipsum ut pretium</a></li>--}}
-                        </ul>
-                    </div>
-                </div> <!-- end of home -->
-            </div>
+        <h3 align="left">👨‍💻 Backend Web Developer / PHP & Laravel</h3>
+        <a href="https://github.com/zaherkhirullah"><h2>🏆 Github Profile Trophy</h2></a>
+        <a href="https://github.com/zaherkhirullah">
+            <img src="https://github-profile-trophy.vercel.app/?username=zaherkhirullah" alt="Zaher Khirullah"/>
+        </a>
+        <div class="my-1">
+            <ul style="list-style:none">
+                <li class="py-1"> - 🔭 I’m currently working on <b><a href="https://imtilakgroup.com">Imtilak Real Estate</a></b> as a <b>Web Developer</b>
+                </li>
+                <li class="py-1"> - 🌱 I’m currently learning <b>Vuejs, GraphQL and Docker</b>
+                </li>
+                <li class="py-1"> - 👨‍💻 All of my projects are available at <a href="https://zaherr.com">https://zaherr.com</a>
+                </li>
+                <li class="py-1"> - 💬 Ask me about <b>PHP and Laravel!</b>
+                </li>
+                <li class="py-1"> - 📫 How to reach me <b><a href="mailto:info@zaherr.com">info@zaherr.com</a></b>
+                </li>
+                <li class="py-1"> - 📄 Know about my experiences <a href="https://zaherr.com/about-us">https://zaherr.com/about-us</a>
+                </li>
+                <li class="py-1"> - ⚡ Fun fact, <b>things aren’t always #000000 and #FFFFFF</b>
+                </li>
+            </ul>
         </div>
     </div>
 
